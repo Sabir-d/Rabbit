88351bcd3aff851c69e65ceee618e83edbdadcf0
dadas <sabir_93d@mail.ru>
2023-10-03 15:40:09 +0300

    Добавьте файлы проекта.

---
+++ Rabbit/Received.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using RabbitMQ.Client;
+using System.Threading;
+using RabbitMQ.Client.Events;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Rabbit
+{
+    public class Received
+    {
+        private string _hostName = "localhost";
+        private string _userName = "sabir";
+        private string _password = "service";
+        private bool _run = false;
+        private Thread thread;
+
+        public void Read(string queues, Func<string, bool> delegat)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = _hostName,
+                UserName = _userName,
+                Password = _password
+            };
+            var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare(
+                queue: queues,
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                bool er = delegat.Invoke(message);
+                if (!_run || !er)
+                {
+                    channel.Close();
+                    connection.Close();
+                    thread.Abort();
+                }
+            };
+            channel.BasicConsume(queue: queues, autoAck: true, consumer: consumer);
+        }
+
+        public void Start(string queues, Func<string, bool> delegat)
+        {
+            _run = true;
+            thread = new Thread(() => Read(queues, delegat));
+            thread.Start();
+        }
+
+        public void Stop(string queues)
+        {
+            _run = false;
+        }
+    }
+}
---
+++ Rabbit/Sending.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using RabbitMQ.Client;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Rabbit
+{
+    public class Sending
+    {
+        private string _hostName = "localhost";
+        private string _userName = "sabir";
+        private string _password = "service";
+
+        public void Send(string message, string queues)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = _hostName,
+                UserName = _userName,
+                Password = _password
+            };
+
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare(
+                queue: queues,
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
+
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(
+                exchange: string.Empty,
+                routingKey: queues,
+                basicProperties: null,
+                body: body
+            );
+        }
+    }
+}
